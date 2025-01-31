a4b0c5502115a651c28beb98872374554091f7f6
Jeron Kor <e0959034@u.nus.edu>
2025-02-01 02:13:16 +0800

    Add mark and unmark commands for task completion tracking

--- src/main/java/LebronJames.java
+++ src/main/java/LebronJames.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class LebronJames {
-    private static final String LINE = "____________________________________________\n";
+    private static final String LINE = "____________________________________________";
     private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
@@ -68,11 +68,36 @@ public class LebronJames {
                 if (taskCount == 0) {
                     System.out.println("No tasks added yet.");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
                         System.out.println((i + 1) + ". " + tasks[i]);
                     }
                 }
                 System.out.println(LINE);
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].markAsDone();
+                        System.out.println(LINE + "Nice! I've marked this task as done:\n  " + tasks[taskIndex] + "\n" + LINE);
+                    } else {
+                        System.out.println(LINE + "Invalid task number!\n" + LINE);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(LINE + "Invalid format! Use: mark X\n" + LINE);
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].markAsNotDone();
+                        System.out.println(LINE + "OK, I've marked this task as not done yet:\n  " + tasks[taskIndex] + "\n" + LINE);
+                    } else {
+                        System.out.println(LINE + "Invalid task number!\n" + LINE);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(LINE + "Invalid format! Use: unmark X\n" + LINE);
+                }
             } else {
                 if (taskCount < MAX_TASKS) {
                     tasks[taskCount] = new Task(input);
