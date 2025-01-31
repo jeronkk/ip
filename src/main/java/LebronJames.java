79723127e4bf42c248c7db553712ed213737ba52
Jeron Kor <e0959034@u.nus.edu>
2025-02-01 03:48:03 +0800

    Refactor task operations into separate methods for better modularity

--- src/main/java/LebronJames.java
+++ src/main/java/LebronJames.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class LebronJames {
     private static final String LINE = "____________________________________________";
     private static final int MAX_TASKS = 100;
+    private static Task[] tasks = new Task[MAX_TASKS];
+    private static int taskCount = 0;
+    private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void printWelcomeMessage() {
         String asciiArt = """
                                                             @@@@@@@@@@@@                                           \s
                                                           @@@@%#*#*##%@@@@@                                        \s
@@ -52,60 +55,70 @@ public class LebronJames {
                 """;
         System.out.println(asciiArt + asciiText);
         System.out.println(LINE + "\n> Hello! I'm Lebron James.\n> What can I do for you?\n" + LINE);
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        Task[] tasks = new Task[MAX_TASKS];
-        int taskCount = 0;
+    public static void printExitMessage() {
+        System.out.println(LINE + "\n> Bye. Hope to see you again soon!\n" + LINE);
+    }
+
+    public static void printTaskList() {
+        System.out.print(LINE);
+        if (taskCount == 0) {
+            System.out.println("No tasks added yet.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
+        System.out.print(LINE);
+    }
+
+    public static void handleMarkTask(String input) {
+        try {
+            int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsDone();
+                System.out.println(LINE + "\nNice! I've marked this task as done:\n  " + tasks[taskIndex] + "\n" + LINE);
+            } else {
+                System.out.println(LINE + "\nInvalid task number!\n" + LINE);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(LINE + "\nInvalid format! Use: mark X\n" + LINE);
+        }
+    }
+
+    public static void handleUnmarkTask(String input) {
+        try {
+            int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsNotDone();
+                System.out.println(LINE + "\nOK, I've marked this task as not done yet:\n  " + tasks[taskIndex] + "\n" + LINE);
+            } else {
+                System.out.println(LINE + "\nInvalid task number!\n" + LINE);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(LINE + "\nInvalid format! Use: unmark X\n" + LINE);
+        }
+    }
+
+    public static void main(String[] args) {
+        printWelcomeMessage();
 
         while (true) {
-            input = scanner.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(LINE + "\n> Bye. Hope to see you again soon!\n" + LINE);
+            String input = scanner.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                printExitMessage();
                 break;
-            } else if (input.equals("list")) {
-                System.out.println(LINE);
-                if (taskCount == 0) {
-                    System.out.println("No tasks added yet.");
-                } else {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
-                    }
-                }
-                System.out.println(LINE);
+            } else if (input.equalsIgnoreCase("list")) {
+                printTaskList();
             } else if (input.startsWith("mark ")) {
-                try {
-                    int taskIndex = Integer.parseInt(input.substring(5)) - 1;
-                    if (taskIndex >= 0 && taskIndex < taskCount) {
-                        tasks[taskIndex].markAsDone();
-                        System.out.println(LINE + "Nice! I've marked this task as done:\n  " + tasks[taskIndex] + "\n" + LINE);
-                    } else {
-                        System.out.println(LINE + "Invalid task number!\n" + LINE);
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println(LINE + "Invalid format! Use: mark X\n" + LINE);
-                }
+                handleMarkTask(input);
             } else if (input.startsWith("unmark ")) {
-                try {
-                    int taskIndex = Integer.parseInt(input.substring(7)) - 1;
-                    if (taskIndex >= 0 && taskIndex < taskCount) {
-                        tasks[taskIndex].markAsNotDone();
-                        System.out.println(LINE + "OK, I've marked this task as not done yet:\n  " + tasks[taskIndex] + "\n" + LINE);
-                    } else {
-                        System.out.println(LINE + "Invalid task number!\n" + LINE);
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println(LINE + "Invalid format! Use: unmark X\n" + LINE);
-                }
-            } else {
-                if (taskCount < MAX_TASKS) {
-                    tasks[taskCount] = new Task(input);
-                    taskCount++;
-                    System.out.println(LINE + "\nadded: " + input + "\n" + LINE);
-                } else {
-                    System.out.println(LINE + "\nTask limit reached! Cannot add more tasks.\n" + LINE);
-                }
+                handleUnmarkTask(input);
+            }  else {
+                System.out.println(LINE + "\n>Invalid command! Try: todo, deadline, event, list, mark, unmark, bye\n" + LINE);
             }
         }
         scanner.close();
