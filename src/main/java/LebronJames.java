1e2334607104754bcf6fc69a19ad63f42fce4a25
Jeron Kor <e0959034@u.nus.edu>
2025-01-31 16:00:24 +0800

    Add scanner to implement echo

--- src/main/java/LebronJames.java
+++ src/main/java/LebronJames.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LebronJames {
     public static void main(String[] args) {
         String asciiArt = "                                            @@@@@@@@@@@@                                            \n"
@@ -44,10 +46,26 @@ public class LebronJames {
         String line = "____________________________________________________________";
         System.out.println(asciiArt + asciiText);
         System.out.println(line);
-        System.out.println("Hello! I'm Lebron James.");
-        System.out.println("What can I do for you?");
+        System.out.println("> Hello! I'm Lebron James.");
+        System.out.println("> What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("> Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println("> " + input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
