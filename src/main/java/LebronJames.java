529a12abce1dde40bd9ba0ee87ae72ac803b0c0b
Jeron Kor <e0959034@u.nus.edu>
2025-02-01 15:53:42 +0800

    Refactor task storage from array to ArrayList for dynamic management

--- src/main/java/LebronJames.java
+++ src/main/java/LebronJames.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class LebronJames {
     private static final String LINE = "____________________________________________";
     private static final int MAX_TASKS = 100;
-    private static Task[] tasks = new Task[MAX_TASKS];
-    private static int taskCount = 0;
+    private static ArrayList<Task> tasks = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
 
     public static void printWelcomeMessage() {
@@ -63,12 +63,12 @@ public class LebronJames {
 
     public static void printTaskList() {
         System.out.print(LINE);
-        if (taskCount == 0) {
+        if (tasks.isEmpty()) {
             System.out.println("\nNo tasks added yet.");
         } else {
             System.out.println("\nHere are the tasks in your list:");
-            for (int i = 0; i < taskCount; i++) {
-                System.out.println((i + 1) + ". " + tasks[i]);
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
             }
         }
         System.out.print(LINE+ "\n");
@@ -77,11 +77,11 @@ public class LebronJames {
     public static void handleMarkTask(String input) throws LebronJamesException {
         try {
             int taskIndex = Integer.parseInt(input.substring(5)) - 1;
-            if (taskIndex < 0 || taskIndex >= taskCount) {
+            if (taskIndex < 0 || taskIndex >= tasks.size()) {
                 throw LebronJamesException.invalidTaskNumber();
             }
-            tasks[taskIndex].markAsDone();
-            System.out.println(LINE + "\nNice! I've marked this task as done:\n  " + tasks[taskIndex] + "\n" + LINE);
+            tasks.get(taskIndex).markAsDone();
+            System.out.println(LINE + "\nNice! I've marked this task as done:\n  " + tasks.get(taskIndex) + "\n" + LINE);
         } catch (NumberFormatException e) {
             throw LebronJamesException.invalidCommand();
         }
@@ -90,11 +90,11 @@ public class LebronJames {
     public static void handleUnmarkTask(String input) throws LebronJamesException {
         try {
             int taskIndex = Integer.parseInt(input.substring(7)) - 1;
-            if (taskIndex < 0 || taskIndex >= taskCount) {
+            if (taskIndex < 0 || taskIndex >= tasks.size()) {
                 throw LebronJamesException.invalidTaskNumber();
             }
-            tasks[taskIndex].markAsNotDone();
-            System.out.println(LINE + "\nOK, I've marked this task as not done yet:\n  " + tasks[taskIndex] + "\n" + LINE);
+            tasks.get(taskIndex).markAsNotDone();
+            System.out.println(LINE + "\nOK, I've marked this task as not done yet:\n  " + tasks.get(taskIndex) + "\n" + LINE);
         } catch (NumberFormatException e) {
             throw LebronJamesException.invalidCommand();
         }
@@ -102,45 +102,44 @@ public class LebronJames {
 
     public static void printTaskAddedMessage(Task task) {
         System.out.println(LINE + "\nGot it. I've added this task:\n" + task +
-                "\nNow you have " + taskCount + " tasks in the list.\n" + LINE);
+                "\nNow you have " + tasks.size() + " tasks in the list.\n" + LINE);
     }
 
     public static void handleTodoTask(String input) throws LebronJamesException {
         if (input.trim().equals("todo")) {
             throw LebronJamesException.emptyTaskDescription();
         }
-        if (taskCount >= MAX_TASKS) {
+        if (tasks.size() >= MAX_TASKS) {
             throw LebronJamesException.taskListFull();
         }
-        tasks[taskCount] = new Todo(input.substring(5));
-        taskCount++;
-        printTaskAddedMessage(tasks[taskCount - 1]);
+        tasks.add(new Todo(input.substring(5)));
+        printTaskAddedMessage(tasks.get(tasks.size() - 1));
     }
 
     public static void handleDeadlineTask(String input) throws LebronJamesException {
-        if (taskCount >= MAX_TASKS) {
+        if (tasks.size() >= MAX_TASKS) {
             throw LebronJamesException.taskListFull();
         }
         String[] parts = input.substring(9).split(" /by ", 2);
         if (parts.length != 2) {
             throw LebronJamesException.invalidCommand();
         }
-        tasks[taskCount] = new Deadline(parts[0], parts[1]);
-        taskCount++;
-        printTaskAddedMessage(tasks[taskCount - 1]);
+        tasks.add(new Deadline(parts[0], parts[1]));
+        printTaskAddedMessage(tasks.get(tasks.size() - 1));
     }
 
     public static void handleEventTask(String input) throws LebronJamesException {
-        if (taskCount >= MAX_TASKS) {
+        if (tasks.size() >= MAX_TASKS) {
             throw LebronJamesException.taskListFull();
         }
         String[] parts = input.substring(6).split(" /from | /to ", 3);
         if (parts.length != 3) {
             throw LebronJamesException.invalidCommand();
         }
-        tasks[taskCount] = new Event(parts[0], parts[1], parts[2]);
-        taskCount++;
-        printTaskAddedMessage(tasks[taskCount - 1]);
+        tasks.add(new Event(parts[0], parts[1], parts[2]));
+        printTaskAddedMessage(tasks.get(tasks.size() - 1));
+    }
+
     }
 
     public static void main(String[] args) {
